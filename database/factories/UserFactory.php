e3dbd38055c64a7d45b99c1b32f0ab0a21a061b7
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-10-24 00:20:57 -0300

    feat: create test to guarantee that a user can be created with an already used username/email

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'username' => fake()->userName(),
+            'is_online' => fake()->boolean(),
+            'profile_picture_url' => fake()->imageUrl(),
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
--- tests/Feature/User/UserControllerTest.php
+++ tests/Feature/User/UserControllerTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
@@ -22,5 +23,23 @@ test('it should create a user', function () {
         'email' => $response['data']['email'],
         'username' => $response['data']['username'],
         'name' => $response['data']['name'],
+    ]);
+});
+
+test('should not create a user with a already used username or email', function () {
+    // Create a user using userFactory
+    $user = User::factory()->create();
+
+    // Tries to create a new user with the user's generated from userFactory data
+    $response = $this->postJson('/api/users', [
+        'email' => $user->email,
+        'username' => $user->username,
+        'name' => $this->faker->name,
+    ]);
+
+    $response->assertStatus(409)->assertJsonStructure([
+        'data',
+        'success',
+        'message'
     ]);
 });
